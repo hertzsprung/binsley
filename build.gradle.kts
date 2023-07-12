b3ef0f20b4ca746698045340efc5d1dbc34ab6d5
James Shaw <js102@zepler.net>
2023-07-12 20:39:19 +0100

    assert on response body in ApiTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,11 +22,14 @@ repositories {
 }
 
 dependencies {
+    implementation("ch.qos.logback:logback-classic:1.4.8")
     implementation("software.amazon.awscdk:aws-cdk-lib:2.87.0")
-    implementation("software.constructs:constructs:10.2.69"))
-    implementation("io.github.cdklabs:cdk-stacksets:0.0.148")
+    implementation("software.constructs:constructs:10.2.69")
     testImplementation(platform("org.junit:junit-bom:5.9.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(platform("org.assertj:assertj-bom:3.24.2"))
+    testImplementation("org.assertj:assertj-core")
     testImplementation(platform("software.amazon.awssdk:bom:2.20.102"))
     testImplementation("software.amazon.awssdk:ssm")
     testImplementation("software.amazon.awssdk:sso")
--- src/test/java/uk/co/datumedge/binsley/ApiTest.java
+++ src/test/java/uk/co/datumedge/binsley/ApiTest.java
@@ -3,12 +3,23 @@ package uk.co.datumedge.binsley;
 import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.services.ssm.SsmClient;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ApiTest {
     @Test
-    void test() {
+    void test() throws IOException, InterruptedException {
         try (SsmClient ssm = SsmClient.create()) {
-            String apiBaseUrl = ssm.getParameter(b -> b.name("/Binsley/ApiBaseUrl")).parameter().value();
-            System.out.println(apiBaseUrl);
+            var apiBaseUri = URI.create(ssm.getParameter(b -> b.name("/Binsley/ApiBaseUrl")).parameter().value());
+
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(HttpRequest.newBuilder().GET().uri(apiBaseUri).build(), HttpResponse.BodyHandlers.ofString());
+            assertThat(response.body()).isEqualTo("{\"message\":\"Hello world\"}");
         }
     }
 }
