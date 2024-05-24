71dd5288c304bbad256c49ba4ddca55223cd7d71
James Shaw <js102@zepler.net>
2024-05-24 19:28:40 +0100

    first attempt at submodule matrix

--- workflow-metrics/build.gradle.kts
+++ workflow-metrics/build.gradle.kts
@@ -18,6 +18,8 @@ repositories {
 }
 
 dependencies {
+    implementation("software.amazon.awscdk:aws-cdk-lib:2.142.1")
+
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ workflow-metrics/src/main/java/uk/co/datumedge/binsley/workflowmetrics/WorkflowMetricsStack.java
@@ -0,0 +1,15 @@
+package uk.co.datumedge.binsley.workflowmetrics;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.sqs.Queue;
+import software.constructs.Construct;
+
+public class WorkflowMetricsStack extends Stack {
+    public WorkflowMetricsStack(final Construct parent, final String id, final StackProps props) {
+        super(parent, id, props);
+
+        Queue.Builder.create(this, "WorkflowMetricsTestQueue")
+                .build();
+    }
+}
